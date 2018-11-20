99fa32bad05fdc4e9399e807158b379dec1e2eac
user1 <gymnopediece@163.com>
2018-11-20 15:46:38 +0800

    go on

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
-def  index(request):
+def index(request):
     return HttpResponse("hello, now you are in django web htmls successfully")
+
+
+def detail(request, question_id):
+    response = " you are looking at detail of %s."
+    return HttpResponse(response % question_id)
+
+
+def results(request, question_id):
+    response = " you are looking at result of %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = " you are voting on %s."
+    return HttpResponse(response % question_id)
