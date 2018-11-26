17f345174bd7600853cb25365bb428bf40c3044c
user1 <gymnopediece@163.com>
2018-11-26 17:12:21 +0800

    go on

--- polls/urls.py
+++ polls/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
 
+
     url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
