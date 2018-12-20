5a0b60c9412bb77e86d9c51ac551748253ef94a7
b <gymnopediece@163.com>
2018-12-20 16:08:49 +0800

    ws

--- Functions/webSockets/demo.py
+++ Functions/webSockets/demo.py
@@ -10,7 +10,7 @@ clients = {}
 # clients
 def notify(message):
     for connection in clients.values():
-        connection.send('%c%c%s' % (0x81, len(message), message))
+        connection.send(bytes('%c%c%s' % (0x81, len(message), message), 'utf-8'))
 
 
 #    thread for  clients operate
@@ -21,57 +21,73 @@ class websocket_thread(threading.Thread):
         self.username = username
 
     def run(self):
-        print('new websocket client joined!')
+
         data = self.connection.recv(1024)
+
         headers = self.parse_headers(data)
+
+        print(data)
         token = self.generate_token(
-            headers['sec-websocket-key'])
-        self.connection.send('\
+            headers['Sec-WebSocket-Key'])
+        self.connection.send(('\
             HTTP/1.1 101 WebSocket Protocol Hybi-10\r\n\
             Upgrade: WebSocket\r\n\
             Connection: Upgrade\r\n\
-            Sec-WebSocket-Accept: %s\r\n\r\n' % token)
+            Sec-WebSocket-Accept: %s\r\n\r\n' % token).encode())
 
         while True:
             try:
                 data = self.connection.recv(1024)
+                print('data' + str(data))
             except socket.error:
-                print('unexpected error')
+                print("unexpected error: ")
                 clients.pop(self.username)
-                break;
+                break
             data = self.parse_data(data)
+            print('data: ' + data)
+            data = '1'
             if len(data) == 0:
                 continue
-            message = self.username + ':  ' + data
+            message = self.username + ": " + data
+            print('message: ' + message)
             notify(message)
 
     def parse_data(self, msg):
-        v = ord(msg([1])) & 0x7f
+        # v = ord(msg[1]) & 0x7f
+        v = 0x7e
+        if (len(msg) > 1):
+            v = ord(msg[1]) & 0x7f
         if v == 0x7e:
             p = 4
-
         elif v == 0x7f:
             p = 10
         else:
             p = 2
-
         mask = msg[p:p + 4]
         data = msg[p + 4:]
         return ''.join([chr(ord(v) ^ ord(mask[k % 4])) for k, v in enumerate(data)])
 
     def parse_headers(self, msg):
         headers = {}
-        header, data = msg.split('\r\n\r\n', 1)
-        for line in header.split('\r\n')[1:]:
-            key, value = line.split(':', 1)
-            headers[key] = value
+
+        header, data = str(msg).split('\\r\\n', 1)
+
+        for line in data.split('\\r\\n')[1:]:
+
+            if len(line) > 3:
+                key, value = line.split(':', 1)
+                headers[key] = value
+
         headers['data'] = data
         return headers
 
     def generate_token(self, msg):
+
         key = msg + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
-        ser_key = hashlib.sha1(key).digest()
+        ser_key = hashlib.sha1(key.encode('utf-8')).digest()
+
         return base64.b64encode(ser_key)
+        # return (ser_key)
 
 
 #  server
