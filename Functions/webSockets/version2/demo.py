6235ba39f78c75f9138467a2bd86ce3da4788cd1
b <gymnopediece@163.com>
2018-12-20 18:39:29 +0800

    ws2

--- Functions/webSockets/version2/demo.py
+++ Functions/webSockets/version2/demo.py
@@ -14,17 +14,18 @@ if __name__ == "__main__":
         print("getting connection")
         # 在这个accept 阻塞
         clientSocket, addressInfo = serverSocket.accept()
+
         print("get connected")
         receivedData = str(clientSocket.recv(2048))
         # print(receivedData)
-        print(receivedData)
+        print('receiveData: '+receivedData)
         entities = receivedData.split("\\r\\n")
         Sec_WebSocket_Key = entities[12].split(":")[1].strip() + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
         print("key ", Sec_WebSocket_Key)
         response_key = base64.b64encode(hashlib.sha1(bytes(Sec_WebSocket_Key, encoding="utf8")).digest())
         response_key_str = str(response_key)
         response_key_str = response_key_str[2:30]
-        print(response_key_str)
+        print('resp key : '+response_key_str)
         response_key_entity = "Sec-WebSocket-Accept: " + response_key_str + "\r\n"
         clientSocket.send(bytes("HTTP/1.1 101 Web Socket Protocol Handshake\r\n", encoding="utf8"))
         clientSocket.send(bytes("Upgrade: websocket\r\n", encoding="utf8"))
