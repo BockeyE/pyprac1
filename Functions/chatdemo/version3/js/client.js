9f8af76fb45559c2e0bfec7abd3ca2551e81b76c
b <gymnopediece@163.com>
2018-12-26 17:29:27 +0800

    ws,v3, open ws manually

--- Functions/chatdemo/version3/js/client.js
+++ Functions/chatdemo/version3/js/client.js
@@ -1,29 +1,30 @@
-window.onload=function(){
-	var socket = new WebSocket('ws://localhost:3369');
+function start() {
+    socket = new WebSocket('ws://localhost:3369');
 
-	socket.onopen = function(){
-		console.log('socket open');
-		$('#send').click(function(){
-			var data = $('#client_text').val();
-			if(data){
-				socket.send(data);
-				$('#client_text').val('');
-				console.log('click');
-			}
-		});
-	}
 
-	socket.onmessage = function(result){
-		$('#server_text').val(result.data);
-		console.log(result.data);
-	}
+    socket.onopen = function () {
+        console.log('socket open');
+        $('#send').click(function () {
+            var data = $('#client_text').val();
+            if (data) {
+                socket.send(data);
+                $('#client_text').val('');
+                console.log('click');
+            }
+        });
+    }
 
-	socket.onclose = function(evt){
-		console.log('socket close');
-	}
+    socket.onmessage = function (result) {
+        $('#server_text').val(result.data);
+        console.log(result.data);
+    }
 
-	socket.onerror = function(evt) { 
+    socket.onclose = function (evt) {
+        console.log('socket close');
+    }
+
+    socket.onerror = function (evt) {
         console.log('error:')
-    };
-}
+    }
 
+}
