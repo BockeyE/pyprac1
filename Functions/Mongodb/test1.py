02dee019c0166f1a094c5eb85f037fa01faf4f9b
b <gymnopediece@163.com>
2019-03-26 16:43:20 +0800

    self

--- Functions/Mongodb/test1.py
+++ Functions/Mongodb/test1.py
@@ -1,9 +1,12 @@
 sql = [
     {'$match': {
-        'trans_details.asset.data.id': transaction_id}},
+        'trans_details.asset.data.id': ""}},
     {'$unwind': '$trans_details'},
     {'$match': {
-        'trans_details.asset.data.id': transaction_id}},
+        'trans_details.asset.data.id': ""}},
     {'$sort': {'height': -1}},
     {'$limit': 1}
 ]
+
+# --file--,指的是这个py本身的路径
+print(__file__)
