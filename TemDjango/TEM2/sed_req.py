45bb49894c615240b5f650c7971b10d85e0b4417
b <gymnopediece@163.com>
2019-05-15 11:05:12 +0800

    req-django

--- TemDjango/TEM2/sed_req.py
+++ TemDjango/TEM2/sed_req.py
@@ -2,9 +2,7 @@ import requests
 
 url = "http://192.168.3.153:8091/v1/handle_tx"
 data = "data"
-headers = {'Connection': 'Keep-Alive',
-           'content-type': 'application/json; charset=utf-8',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063'}
-
+# headers = {'content-type': 'application/json; charset=utf-8'}
+headers = None
 resp = requests.post(url=url, data=data, headers=headers)
 print(resp.text)
