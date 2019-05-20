e3780b8b6087af9728e2c92b2e19c199f5727564
b <gymnopediece@163.com>
2019-05-20 17:00:16 +0800

    exec

--- virtualenv/RestrictEXEC/exec_test.py
+++ virtualenv/RestrictEXEC/exec_test.py
@@ -44,6 +44,8 @@ exec('exec(\'print(sqrt(9))\')', {'sqrt': no, 'pow': pow})
 用户只传递globals字典时，如果传递非空字典，exec内只能访问内置函数(__builtins__)、字典中包含的函数和变量（变量只是一个拷贝，内部修改了不会影响外面的变量，外面修改了不会影响内部的变量）。
 用户同时传递globals字典和locals字典时，使用方式同上，但是如果globals字典设置{'\_\_builtins\_\_' : None}，locals字典设置其他函数和方法是，就不能访问内置函数(__builtins__)。注：这一点有点疑问！
 
+执行之后exec语句中的func和var会以字典形式存在locals中
+
 exec执行的字符串中定义了函数和变量，是会保存在传递进去的globals字典中的，所以可以使用该字典获取字符串内的函数，并调用。
 同时传递globals字典和locals字典时，locals字典优先级高。
 '''
