b037e0bb383f67f7b07728320fb4faa8fb8ac395
b <gymnopediece@163.com>
2019-05-23 11:38:53 +0800

    exec

--- RestrictedPython/exec_test.py
+++ RestrictedPython/exec_test.py
@@ -3,8 +3,8 @@ import pymongo
 from ssl import CERT_REQUIRED
 from RestrictedPython.Guards import safe_globals
 
-# with open('fullc_clear.py', 'r', encoding='UTF-8') as f:
-with open('fullc.py', 'r', encoding='UTF-8') as f:
+with open('fullc_clear.py', 'r', encoding='UTF-8') as f:
+    # with open('fullc.py', 'r', encoding='UTF-8') as f:
     meta2 = f.read()
 
 mylocals = {'AutoReconnect': pymongo.errors.AutoReconnect,
@@ -13,9 +13,15 @@ mylocals = {'AutoReconnect': pymongo.errors.AutoReconnect,
             'MongoClient': pymongo.MongoClient,
             'ConnectionFailure': pymongo.errors.ConnectionFailure,
             'ConfigurationError': pymongo.errors.ConfigurationError,
-            'CERT_REQUIRED': CERT_REQUIRED}
-my = {}
+            'CERT_REQUIRED': CERT_REQUIRED,
+            '__import__': None}
+
+gs = globals()
+# my = {}
+my = gs
+safe_globals['__loader__'] = gs['__loader__']
+safe_globals['__builtins__'] = gs['__builtins__']
 compile(meta2, '<string>', 'exec')
-exec(meta2, safe_globals, my)
-print(my)
-print(my['main']())
+exec(meta2, mylocals)
+# print(my)
+# print(my['main']())
