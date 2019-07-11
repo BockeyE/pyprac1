234079836769558ef8c8e5b7b683c121def3b8a0
b <gymnopediece@163.com>
2019-07-11 17:14:26 +0800

    2

---
+++ Functions/GetLatest2/package.py
@@ -0,0 +1,21 @@
+def package(p):
+    if p % 2 == 1:
+        return 1
+    if p & (p - 1) == 0:
+        while p > 512:
+            p = p - 512
+        print(p)
+        return p
+    print(p)
+    p = p - 1
+    print(p)
+    p = p | (p >> 16)
+    print(p)
+    p = p | (p >> 8)
+    p = p | (p >> 4)
+    p = p | (p >> 2)
+    p = p | (p >> 1)
+    print(p + 1)
+
+
+package(14)
