7c2d265813cce78e5a7b617e0f27361cb8db28f2
b <gymnopediece@163.com>
2019-07-11 17:24:37 +0800

    2

--- Functions/GetLatest2/package.py
+++ Functions/GetLatest2/package.py
@@ -1,22 +1,31 @@
 def package(p):
     if p % 2 == 1:
+        print("ended with 1")
         return 1
     if p & (p - 1) == 0:
         while p > 512:
             p = p - 512
         print(p)
+        print("ended with power")
         return p
-    print(p)
+    m = get_power(p) >> 1
+    print(m)
+    package(p - m)
+
+
+def get_power(p):
     p = p - 1
-    print(p)
     p = p | (p >> 16)
-    print(p)
     p = p | (p >> 8)
     p = p | (p >> 4)
     p = p | (p >> 2)
     p = p | (p >> 1)
-    print(p + 1)
+    return p + 1
 
 
 package(14)
 package(18)
+package(20)
+package(24)
+package(28)
+package(36)
