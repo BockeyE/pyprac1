fe5286b34744e8be26f4b0e615c9c1a10b3d9832
b <gymnopediece@163.com>
2019-10-30 17:13:24 +0800

    str tool

--- fileread/FileRead.py
+++ fileread/FileRead.py
@@ -1,13 +1,19 @@
+import base64
 import hashlib
 
 f = open("C:\ZZBK\sqs.pdf", 'rb', True)
 data=f.read()
 f.close()
 print(":::::")
-print(data)
+# print(data)
 sha256obj = hashlib.sha256()
-sha256obj.update("AAA".encode("UTF-8"))
+
+# sha256obj.update(base64.b64encode(data))
+sha256obj.update((data))
+
 hash_value = sha256obj.hexdigest()
+# sha256obj.update("AAA".encode("UTF-8"))
+
 
 # print(CalcFileSha256())
 # 61e36ec48758e547b81d2434670a57e0
