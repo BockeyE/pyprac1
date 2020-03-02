2b427b37b084d4acd13eb0c5894445443f3d9165
b <gymnopediece@163.com>
2020-03-02 18:40:49 +0800

    pycon

---
+++ Functions/MysqlConnector/pycon.py
@@ -0,0 +1,14 @@
+import pymysql
+
+# 创建一个连接对象，再使用创建游标
+con = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='123456', db='mysql')
+cursor = con.cursor()
+
+# 执行一个SQL语句
+sql = "select * from user"
+cursor.execute(sql)
+
+# 从游标中取出所有记录放到一个序列中并关闭游标
+result = cursor.fetchall()
+print(result)
+cursor.close()
