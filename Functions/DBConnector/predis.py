7a93b8ecd0ed4ed5c040a7694796d388a0ae1880
b <gymnopediece@163.com>
2020-03-03 12:15:45 +0800

    dbs

--- Functions/DBConnector/predis.py
+++ Functions/DBConnector/predis.py
@@ -1,5 +1,7 @@
 import redis
-conn = redis.Redis(host='127.0.0.1', port=6379)
+from redis import Redis
+
+conn = Redis(host='127.0.0.1', port=6379)
 # 可以使用url方式连接到数据库
 # conn = Redis.from_url('redis://@localhost:6379/1')
 conn.set('name', 'LinWOW')
